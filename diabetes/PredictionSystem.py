4a5047124f198af8fe9bda5a5053f043ea47cbba
H4K4M <ha-kam009@hotmail.com>
2023-01-03 12:17:59 +0300

    Sending email

--- diabetes/PredictionSystem.py
+++ diabetes/PredictionSystem.py
@@ -12,19 +12,21 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from csv import writer
 from datetime import date
-import pyodbc 
-import smtplib, ssl
+import pyodbc
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=H4K4MSPC\SQLEXPRESS;'
-                      'Database=Diabetes_DB;'
+                      'Database=DIABETES;'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor() 
     
 
 
-diabetes_dataset = pd.read_csv('D:\Documents(D)\diabetes\diabetes.csv')
+diabetes_dataset = pd.read_csv(r"D:\Documents(D)\Git\DiabetsDisease\diabetes\diabetes.csv")
 # separating the data and labels
 X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
 Y = diabetes_dataset['Outcome']
@@ -52,13 +54,13 @@ st.title('Diyabet Hastalığı Tespit Sistemi')
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    Ad = st.text_input('Ad')
+    Ad = st.text_input('Ad*')
     
 with col2:
-    Soyad = st.text_input('Soyad')
+    Soyad = st.text_input('Soyad*')
 
 with col3:
-    Eposta = st.text_input('E-posta')
+    Eposta = st.text_input('E-posta*')
 
     
 st.header("Diyabet Test Bilgileri")
@@ -94,93 +96,125 @@ diab_diagnosis = ''
 
 agree = st.checkbox('Bilgilerimin işlenmesini onaylıyorum. (Girilen bilgiler üçüncü kişiler ile paylaşılmayacaktır.)')
 
+
+
+def SendEmail():
+    email_sender = 'diabetespredictionn@gmail.com'
+    email_password = 'uppcuyiimiujdjdj'
+    email_receiver = str(Eposta)
+    
+    result = ""
+    if prediction[0] == 0:
+        result = "negatiftir"
+    else:
+        result = "Pozitifttir"
+        
+    subject = 'Diyabet Test Sonucu'
+    body = """Sayın {0} {1} 
+    {2} tarihinde yaptırmış olduğunuz diyabet testinizin sonucu {3}.
+    Bize gönderdiğiniz tüm bilgiler:
+        E-posta:{4}
+        Gebelik Sayısı:{5}
+        Glikoz Seviyesi:{6}
+        Kan Basıncı:{7}
+        Deri Kalınlığı:{8}
+        Insulin Seviyesi:{9}
+        Vücut Kitle Endeksi:{10}
+        Diyabet Soyağacı:{11}
+        Yaş:{12}
+        
+        Diyabet hastalığı hakkında bilgi almak için şu https://www.turkdiab.org/diyabet-hakkinda-hersey.asp?lang=TR&id=46 ulaşabilirsiniz
+    """.format(Ad, Soyad, date.today(), result, Eposta, Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age)
+    
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content(body)
+    
+    context = ssl.create_default_context()
+    
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
+
 if agree:
     
     # creating a button for Prediction
     if st.button('Test Sonucunu Görüntüleyin'):
-        #diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
-        diab_prediction = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
-        # changing the input_data to numpy array
-        input_data_as_numpy_array = np.asarray(diab_prediction)
-
-        # reshape the array as we are predicting for one instance
-        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-        # standardize the input data
-        std_data = scaler.transform(input_data_reshaped)
-        prediction = classifier.predict(std_data)
-    
-        if (prediction[0] == 1):
-            diab_diagnosis = 'Diyabet Hastasısınız'
+        if Ad == "" and Soyad == "" and Eposta == "":
+            diab_diagnosis = 'Gerekli alanları doldurun'
             st.error(diab_diagnosis)
-        else:
-            diab_diagnosis = 'Şeker Hastası Değilsiniz'
-            st.success(diab_diagnosis)
-      
             
-        input_list = diab_prediction
-        input_list.append(prediction[0])
-        
-        cursor.execute("SELECT eposta from Hasta where eposta=?",Eposta )
-        eposta = cursor.fetchall()
-        conn.commit()
-        
-        if len(eposta) == 0:
-            #Add Hasta to DB_TBL
-            cursor.execute("EXEC HastaEkle @ad=?, @soyad=?, @eposta = ?",Ad,Soyad,Eposta)
-            conn.commit()
-        
-        cursor.execute("SELECT HastaID from Hasta where eposta=?",Eposta )
-        HastaID = cursor.fetchall()
-        conn.commit()
-        
-        HastaID = HastaID[0][0]
-        result = int(prediction[0])
-        date = date.today()
-        #Add Hasta to DB_TBL
-        params = (HastaID, Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age, str(date), result)
-        cursor.execute("EXEC TestEkle @hastaID=?, @gebelik=?, @glikoz=?, @kan=?, @deri=?, @insulin=?, @vke=?, @soyagac=?, @yas=?, @tarih=?, @sonuc=?",params )
-        conn.commit()
-        
-        
-        # Sending Email
-        port = 465  # For SSL
-        smtp_server = "smtp.gmail.com"
-        sender_email = "diabetespredictionn@gmail.com"  # Enter your address
-        receiver_email = Eposta  # Enter receiver address
-        password = "diyabet1234@"
-        
-        
-        cursor.execute("EXEC HastaTestGetir @eposta = ?",Eposta)
-        GetData  = cursor.fetchall()
-        conn.commit()
-        TestID = GetData[-1][0]
-        Name = GetData[-1][1]
-        date = GetData[-1][2]
-        TestResult = ""
-        if GetData[-1][-1] == 1:           
-            TestResult = "Pozitiftir."
         else:
-            TestResult = "negatiftir."
+            #diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+            diab_prediction = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
+            # changing the input_data to numpy array
+            input_data_as_numpy_array = np.asarray(diab_prediction)
+    
+            # reshape the array as we are predicting for one instance
+            input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+            # standardize the input data
+            std_data = scaler.transform(input_data_reshaped)
+            prediction = classifier.predict(std_data)
         
-        message = "Sayın " + str(Name) +", "+ str(date) + " tarihinde yaptırmış olduğunuz "+ str(TestID) + " numaralı son testin sonucu "+str(TestResult)
-        
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
-        
-        
-        
+            if (prediction[0] == 1):
+                diab_diagnosis = 'Diyabet Hastasısınız'
+                st.error(diab_diagnosis)
+                SendEmail()
+            else:
+                diab_diagnosis = 'Şeker Hastası Değilsiniz'
+                st.success(diab_diagnosis)
+                SendEmail()
+          
+                
+            input_list = diab_prediction
+            input_list.append(prediction[0])
+            
+            cursor.execute("SELECT eposta from Hasta where eposta=?",Eposta )
+            eposta = cursor.fetchall()
+            conn.commit()
+            
+            if len(eposta) == 0:
+                #Add Hasta to DB_TBL
+                cursor.execute("EXEC HastaEkle @ad=?, @soyad=?, @eposta = ?",Ad,Soyad,Eposta)
+                conn.commit()
+            
+            cursor.execute("SELECT HastaID from Hasta where eposta=?",Eposta )
+            HastaID = cursor.fetchall()
+            conn.commit()
+            
+            HastaID = HastaID[0][0]
+            result = int(prediction[0])
+            date = date.today()
+            #Add Hasta to DB_TBL
+            params = (HastaID, Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age, str(date), result)
+            cursor.execute("EXEC TestEkle @hastaID=?, @gebelik=?, @glikoz=?, @kan=?, @deri=?, @insulin=?, @vke=?, @soyagac=?, @yas=?, @tarih=?, @sonuc=?",params )
+            conn.commit()
+            
+    
+        # Open file in append mode
+            with open('diabetes.csv', 'a+', newline='') as write_obj:
+                # Create a writer object from csv module
+                csv_writer = writer(write_obj)
+                # Add contents of list as last row in the csv file
+                csv_writer.writerow(input_list)
+    
+    
 
-    # Open file in append mode
-        with open('diabetes.csv', 'a+', newline='') as write_obj:
-            # Create a writer object from csv module
-            csv_writer = writer(write_obj)
-            # Add contents of list as last row in the csv file
-            csv_writer.writerow(input_list)
     
     
     
-
-else:
-    diab_diagnosis = 'Gerekli alanları doldurun'
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
